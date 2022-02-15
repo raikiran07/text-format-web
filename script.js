38f9ec7b3a2ae65e3e76e09dac3079052ca6dace
raikiran07 <raikiran925@gmail.com>
2022-02-16 00:39:05 +0530

    new feature added

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const capital = document.getElementById("capital");
 const allCap = document.getElementById("allCap");
 const removeNumber = document.getElementById("removeNumber");
 const removeSpecial = document.getElementById("removeSpecial");
+const capitalNewSentence = document.getElementById("capital-new");
+const upperCase = document.getElementById("uppercase");
+const lowerCase = document.getElementById("lowercase");
 const submitBtn = document.getElementById("enter");
 const userInput = document.getElementById("username");
 const resetBtn = document.getElementById("reset");
@@ -32,14 +35,36 @@ function formatText(){
             
             
         }
+        if(upperCase.checked){
+            str = str.toUpperCase();
+            console.log(str);
+        }
+        if(lowerCase.checked){
+            str = str.toLowerCase();
+            console.log(str);
+        }
+    
         if(removeNumber.checked){
             str = str.replace(/[0-9]/g,"");
         }
         if(removeSpecial.checked){
              str = str.replace(/[^a-zA-Z0-9 ]/g, "");
         }
-    
-        displayResult.style.display="block";
+        if(capitalNewSentence.checked){
+            const strArr = str.split(" ");
+            strArr[0] = strArr[0].replace(strArr[0][0],strArr[0][0].toUpperCase());
+            for(let i=0;i<strArr.length;i++){
+                    if(strArr[i].includes('.')&&i<strArr.length-1){
+                        strArr[i+1] = strArr[i+1].replace(strArr[i+1][0],strArr[i+1][0].toUpperCase());
+
+                        }
+            str = strArr.join(" ");
+                        }
+            
+        }
+
+        
+        displayResult.style.opacity="1";
         result.innerHTML = str;
     
 
